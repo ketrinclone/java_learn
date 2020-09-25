100821999e7ca1c265f93afd73d07289a955fc9c
ketrin clone <ketrinclone@telegmail.com>
2020-09-25 11:46:26 +0700

    update

--- index.java
+++ index.java
@@ -1,5 +1,7 @@
-public class MyClass {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
-  }
+public class index {
+  public static void main(String[] args){
+    System.out.println("Heloo World");
+    System.out.println("You can try this program to edit and fork it for hacktober fest!");
+    System.out.println("OwO!! Love youuu all!");
+  }//End of main
+}//End of FirstJavaProgram Class
